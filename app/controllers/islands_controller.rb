dee7df0094ec3c08488905ca0354c0c3fbd7ecdd
Dimby Rakotomalala <dimby777@gmail.com>
2020-02-17 17:58:35 +0100

    Added the view for Island

---
+++ app/controllers/islands_controller.rb
@@ -0,0 +1,49 @@
+class IslandsController < ApplicationController
+  def index
+    @islands = Island.all
+  end
+
+  def show
+    @island = Island.find(params[:id])
+  end
+
+  def new
+    @island = Island.new
+  end
+
+  def create
+    @island = Island.new(island_params)
+    if @island.save
+      redirect_to island_path(@island)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @island = Island.find(params[:id])
+  end
+
+  def update
+    @island = Island.find(params[:id])
+    @island.update(params[:id])
+    if @island.save
+      redirect_to island_path(@island)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @island = Island.find(params[:id])
+    @island.destroy
+
+    redirect_to islands_path
+  end
+
+  private
+
+  def island_params
+    params.require(:island).permit(:name, :address, :price, :number_of_guests, :user_id)
+  end
+end
