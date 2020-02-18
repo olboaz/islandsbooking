e9d05989b8a0946fbf417b508960de7902b675fa
Julien Pierlot <pierlot.julien@gmail.com>
2020-02-18 14:33:06 +0100

    form booking

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -8,6 +8,7 @@ class IslandsController < ApplicationController
 
   def show
     @island = Island.find(params[:id])
+    @booking = Booking.new
   end
 
   def new
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,11 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+import { updatePrice } from '../custom/booking_form';
+
+const nbOfNightsInput = document.getElementById('booking_nb_of_nights');
+const bookingForm = document.getElementById('booking-form');
+
+if (bookingForm) {
+  nbOfNightsInput.addEventListener('keyup', updatePrice);
+};
