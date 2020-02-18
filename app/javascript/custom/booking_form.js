7ff1c200221a5aa6281a61bcc16442d7e4d77b49
Julien Pierlot <pierlot.julien@gmail.com>
2020-02-18 16:15:16 +0100

    fix submit booking form

--- app/javascript/custom/booking_form.js
+++ app/javascript/custom/booking_form.js
@@ -1,13 +1,18 @@
 const updatePrice = (event) => {
+  const submitButton = document.getElementById('booking-submit');
   const price = parseFloat(document.getElementById('price').dataset.price);
-  // console.log(parseFloat(document.getElementById('price').dataset.price) * 10)
   let totalPriceInput = document.getElementById('total-price');
+
   let nights = parseInt(event.currentTarget.value, 10);
   if (isNaN(nights)) {
     totalPriceInput.setAttribute("value", 0);
   } else {
-    // totalPriceInput.value = price * nights;
     totalPriceInput.setAttribute("value", price.toFixed(2) * nights);
+  }
+  if (parseInt(totalPriceInput.getAttribute("value"), 10) === 0) {
+    submitButton.disabled = true;
+  } else {
+    submitButton.disabled = false;
   }
 }
 
