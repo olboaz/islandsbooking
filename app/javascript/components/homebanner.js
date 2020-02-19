a4eef88a55ce9794f8e51a450087d703fb3dd5f1
Julien Pierlot <pierlot.julien@gmail.com>
2020-02-19 13:51:49 +0100

    fix typedjs error

--- app/javascript/components/homebanner.js
+++ app/javascript/components/homebanner.js
@@ -1,10 +1,13 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["Book your dream island"],
-    typeSpeed: 50
-  });
+  const homeText = document.getElementById('banner-typed-text');
+  if (homeText) {
+    new Typed('#banner-typed-text', {
+      strings: ["Book your dream island"],
+      typeSpeed: 50
+    });
+  }
 }
 
 export { loadDynamicBannerText };
