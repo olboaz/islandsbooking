3d0f9bae8243bf1d034f29c372395c530a2d61ae
Paul Heilweck <paul.heilweck@edu.escpeurope.eu>
2020-02-19 12:24:13 +0100

    debug console + navbar

--- app/javascript/components/homebanner.js
+++ app/javascript/components/homebanner.js
@@ -2,8 +2,8 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Book your dream island to stay !"],
-    typeSpeed: 50,
+    strings: ["Book your dream island"],
+    typeSpeed: 50
   });
 }
 
