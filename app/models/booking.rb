709cbec681117891ae8c7675acc21ebd715db4d6
Dimby Rakotomalala <dimby777@gmail.com>
2020-02-20 17:01:19 +0100

    Ajout des reviews dans la page utilisateir + récapitulatif sur la show des iles

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :island
   belongs_to :user
+  has_many :reviews, dependent: :destroy
 
   validates :start_date, :end_date, presence: true, availability: true
   validate :check_date_from_today, :end_date_after_start_date
