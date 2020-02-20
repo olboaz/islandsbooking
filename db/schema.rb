1941d81bdfc8af357962bd7378c3dc1af8048d06
Julien Pierlot <pierlot.julien@gmail.com>
2020-02-20 10:53:56 +0100

    status pending by default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_152010) do
+ActiveRecord::Schema.define(version: 2020_02_20_094656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_02_19_152010) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "status", default: "pending"
     t.index ["island_id"], name: "index_bookings_on_island_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
