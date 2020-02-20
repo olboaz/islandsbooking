6c59033d6fdb29fe4c66556401cba61fa4ff7711
Julien Pierlot <pierlot.julien@gmail.com>
2020-02-20 10:15:19 +0100

    clean js files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,18 +6,11 @@ require("channels")
 import "bootstrap";
 import { loadDynamicBannerText } from '../components/homebanner';
 import { toggleDropdownMenu } from "../components/navbar";
-import { updatePrice } from '../custom/booking_form';
 import { initFlatpickr } from '../plugins/init_flatpickr';
 
 document.addEventListener('turbolinks:load', () => {
   loadDynamicBannerText();
   toggleDropdownMenu();
   initFlatpickr();
-
-  const nbOfNightsInput = document.getElementById('booking_nb_of_nights');
-  const bookingForm = document.getElementById('booking-form');
-  // if (bookingForm) {
-  //   nbOfNightsInput.addEventListener('input', updatePrice);
-  // };
 });
 
