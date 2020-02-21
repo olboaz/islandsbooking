2a39ae8582e89c4db7fa1bf8e4a90961df461b83
Paul Heilweck <paul.heilweck@edu.escpeurope.eu>
2020-02-21 12:10:56 +0100

    map cards added

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -35,7 +35,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 4, duration: 0 });
+  map.fitBounds(bounds, { padding: 20, maxZoom: 3, duration: 0 });
 };
 
 const initMapbox = () => {
