a52ff0b6898177943506a60ba5a8ef07eb98aaf5
Julien Pierlot <pierlot.julien@gmail.com>
2020-02-21 12:59:50 +0100

    added pending bookings on user show + validation update

--- app/models/island.rb
+++ app/models/island.rb
@@ -13,7 +13,7 @@ class Island < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
 
   def unavailable_dates
-    bookings.pluck(:start_date, :end_date).map do |range|
+    bookings.where.not(status: 'refused').pluck(:start_date, :end_date).map do |range|
       { from: range[0], to: range[1] }
     end
   end
