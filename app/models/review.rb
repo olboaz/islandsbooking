781241ed56c405f8fe4911b622df6a5bf216c835
Julien Pierlot <pierlot.julien@gmail.com>
2020-02-21 14:37:12 +0100

    fix review link display + modify validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :booking
   belongs_to :user
-  validates :content, presence: true, length: { minimum: 15, maximum: 20 }
+  validates :content, presence: true, length: { minimum: 5, maximum: 100 }
   validates :rating, presence: true, inclusion: (1..5)
 end
